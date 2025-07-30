728f8498d982dc584d4143740d29251552a63ffc
jayeshkumarsingh11 <jayeshkumarsingh11@gmail.com>
2025-07-30 23:40:48 +0530

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+import { useNavigate } from "react-router-dom";
+
+const Footer = () => {
+  const navigate = useNavigate();
+  return (
+    <footer id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img className="logo" src={assets.logo} alt="logo" />
+          <p>
+          Explore a world of taste with our thoughtfully prepared menu, featuring exceptional dishes for every craving. We use only the best ingredients and expert techniques to bring you a truly elevated and satisfying meal, every single time.
+          </p>
+          <div className="footer-social-icons">
+            <a href="https://www.facebook.com">
+              <img src={assets.facebook_icon} alt="facebook" />
+            </a>
+            <a href="https://www.twitter.com">
+              <img src={assets.twitter_icon} alt="twitter" />
+            </a>
+            <a href="https://www.linkedin.com">
+              <img src={assets.linkedin_icon} alt="linkedin" />
+            </a>
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li onClick={() => navigate("/")}>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+1-123-456-7890</li>
+            <li>contact@foodio.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">Copyright 2024 © Foodio.com</p>
+    </footer>
+  );
+};
+
+export default Footer;
