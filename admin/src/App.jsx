14a299942a3f05018000a3f81290f29407f238e4
jayeshkumarsingh11 <jayeshkumarsingh11@gmail.com>
2026-06-03 20:40:43 +0530

    Food Delivery Web App

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/navbar/navbar'
 
 const App = () => {
-  const url = "http://localhost:4000"
+  const url = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000"
   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   const toggleMenu = () => {
